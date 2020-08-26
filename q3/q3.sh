f90279f5d74868eaa2298eaa8ac2727992e402ce
ayush-qubit <ayushsharma1198@gmail.com>
2020-08-26 22:47:08 +0530

    commit

--- q3/q3.sh
+++ q3/q3.sh
@@ -4,10 +4,11 @@ VAR3="$VAR1$VAR2"
 COUNT=0
 touch output.txt
 find $VAR3 -type f -name "*.txt">output.txt
+sed -i '$d' output.txt
 while IFS= read -r line
 do
-	num=$(wc -l<$line)
-	COUNT=$((COUNT+num))
+	NUM=$(grep . $line | wc -l)
+	COUNT=$((COUNT+NUM))
 done < output.txt
 rm output.txt
 echo $COUNT
